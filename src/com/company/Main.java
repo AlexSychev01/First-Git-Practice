882613746546f64aaa4a35f5181183e83229c99a
AlexSychev01 <alex39ksg@mail.ru>
2020-05-16 11:13:26 +0200

    Added 5 lines in feature2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Git worflow");
+        System.out.println("Feature two line 1");
+        System.out.println("Feature two line 2");
+        System.out.println("Feature two line 3");
+        System.out.println("Feature two line 4");
+        System.out.println("Feature two line 5");
+
 	// write your code here
     }
 }
