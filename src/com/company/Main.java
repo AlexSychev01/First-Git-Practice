fa41a92aba014c17b0fe6dcf4544ce04190b210a
AlexSychev01 <alex39ksg@mail.ru>
2020-05-16 11:09:32 +0200

    Added 3 lines in feature1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Git worflow");
+        System.out.println("First commit");
+        System.out.println("Second commit");
+        System.out.println("Third commit");
 	// write your code here
     }
 }
